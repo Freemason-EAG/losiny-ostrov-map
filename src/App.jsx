a5b0e76a96d86efd9446cea8cc7e064b5e59773f
Freemason-EAG <egoyan.arutyun@mail.ru>
2026-04-20 18:28:30 +0300

    Setup: Vite + React + PWA + maplibre-gl

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const App = () => {
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <h1>Лосиный остров</h1>
+      <p>Карта будет здесь</p>
+    </div>
+  )
+}
+
+export default App
